528babda784f31df23d442da9d46658a0a2e133d
ZloyA <103591605+ZloyA@users.noreply.github.com>
2022-07-04 10:39:34 +0500

    Add files via upload

---
+++ Пайтон/питон, например/ДЗ 3.py
@@ -0,0 +1,44 @@
+# a = input("Введите имя: ") # 20 задача
+# lent = len(a)
+# print(lent)
+
+# b = input("Введите имя: ") # 21 задача
+# c = input("Введите фамилию: ")
+# d = b + " " + c
+# lenn = len(d)
+# lennn = lenn - 1
+# print(d, lennn)
+
+# e = input("Введите имя маленькими буквами: ") # 22 задача
+# ee = input("Введите фамилию маленькими буквами: ")
+# eee = e + " " + ee
+# f = eee.upper()
+# print(f)
+
+# ff = input("Введите строку из стихотворения: ") # 23 задача
+# f3 = len(ff)
+# print("Длина строки: ", f3)
+# f4 = int(input("Введите номер символа для начала вывода: "))
+# f5 = int(input("Ведите номер символа для окончания вывода: "))
+# print (ff[f4:f5])
+
+# g = input("Введите имя: ") # 24 задача
+# g1 = len(g)
+# if g1 < 5:
+#     g2 = input("Введите фамилию: ")
+#     g3 = g + g2
+#     g4 = g3.upper()
+#     print(g4)
+# else:
+#     g5 = g.lower()
+#     print(g5)
+
+# word = input("Введите слово на английском: ")  # 25 задача(скопирована из учебника)
+# first = word[0]
+# length = len(word)
+# rest = word[1:length]
+# if first != "a" and first != "e" and first != "i" and first != "o" and first != "u":
+#     newword = rest + first + "ay"
+# else:
+#     newword = word + "way"
+# print(newword.lower())
